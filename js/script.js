374adb5657a9149d4f5f6f70a55448e4f99d8a50
JonathanADB <jhonnysoy1@gmail.com>
2024-02-19 12:08:10 +0100

    termino boton hamburguesa

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 
+//script boton hamburguesa
+document.querySelector(".bars_menu").addEventListener("click", animateBars);
+
+let line1_bars = document.querySelector(".line1_bars-menu");
+let line2_bars = document.querySelector(".line2_bars-menu");
+let line3_bars = document.querySelector(".line3_bars-menu");
+
+function animateBars(){
+    line1_bars.classList.toggle("activeline1_bars-menu");
+    line2_bars.classList.toggle("activeline2_bars-menu");
+    line3_bars.classList.toggle("activeline3_bars-menu");
+}
+
+//script nav dentro del boton hamburguesa
+document.querySelector(".bars_menu").addEventListener("click", animateMenu);
+
+let ulElement = document.querySelector(".responsive");
+
+function animateMenu () {
+    ulElement.classList.toggle("show-menu");
+
+}
+
+
 
 //script para la fecha
  const $fecha = document.querySelector('.fecha');
@@ -25,6 +49,9 @@ setInterval(() => {
     digitalClock()
 
 }, 1000);
+
+
+
 
 const productos = [
     
