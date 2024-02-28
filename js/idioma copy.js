4789393f4fba0db3cf132f5ceb950a744c44e35b
jhonsdb <davilajhon08@gmail.com>
2024-02-28 09:54:09 +0100

    configurar menu

--- js/idioma copy.js
+++ js/idioma copy.js
@@ -1,16 +1,16 @@
 const flgasElement = document.getElementById("flags");
 
-const texstToChange = document.querySelectorAll("[data-section");
+const texstToChange = document.querySelectorAll("[data-value");
 
 const changeLanguages =  async language =>{
 const requestJson = await fetch(`./languages/${language}.json`)
 const texts = await requestJson.json();
 
 for (const textToChange of texstToChange){
-    const section = textToChange.dataset.section;
+    
     const value = textToChange.dataset.value;
 
-    textToChange.innerHTML=texts[section][value];
+    textToChange.innerHTML=texts[value];
 }
 
 
