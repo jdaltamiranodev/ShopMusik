cb30bcd487fe6fb8024ce2be10f309330d75f2e6
David Altamirano <jdaltamiranodev@gmail.com>
2024-02-28 13:25:11 +0100

    Se actualiza

--- js/script.js
+++ js/script.js
@@ -23,34 +23,6 @@ function animateMenu () {
 }
 
 
-
-//script para la fecha
- const $fecha = document.querySelector('.fecha');
-
-function digitalClock(){
-    let f = new Date(),
-    dia = f.getDate(),
-    mes = f.getMonth(),
-    anio = f.getFullYear(),
-    diaSemana = f.getDay();
-    
-    dia = ('0' + dia).slice(-2);
-
-    let semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves','V', 'Sábado'];
-    let showSemana = (semana[diaSemana]);
-
-    let mesAnio = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
-    let showMes = (mesAnio[mes]);
-
-    document.querySelector('.fecha').innerHTML = `${showSemana} ${dia} ${showMes} ${anio}`
-} 
-
-setInterval(() => {
-    digitalClock()
-
-}, 1000);
-
-
 const productos = [
     
     // Instrumentos de cuerda
